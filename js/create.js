fcd5fdf71b3059159bf2cc5472f7dd843dd659d3
ibrahem hassan <hibrahem266@gmail.com>
2022-10-10 17:30:59 +0200

    full

--- js/create.js
+++ js/create.js
@@ -1,1 +1,11 @@
 
+var mobile =document.getElementsByClassName("mobile");
+const desktop =document.getElementsByClassName("desktop");
+
+
+ function hover(){
+  console.log("hi");
+ }
+mobile.onclick=hover;
+
+
